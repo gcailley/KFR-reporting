8a8236d040cfc3384ca76e880d36c6d371aecd9d
gcailley <gregory.cailley@gmail.com>
2019-06-16 21:18:54 +0200

    mise à jour rapport ajout benevolat, materiels

--- rapportFinancier/export_KFR_Bilan.R
+++ rapportFinancier/export_KFR_Bilan.R
@@ -1,21 +1,21 @@
 # Questions à valider : 
-# NB : durée des API Key
-# Calcul du nombre d'adhérents : -1 si on enlève Greg dans le nombre d'adhérents
-# Jointure avec les cotisations id ? Pour le moment le compteur est effectué sur le libellé de la cotisation
-# Catégorie trésorerie : manque les intérets bancaires par rapport au budget, le site n'était pas provisionné l'année précédente
+# NB : duree des API Key
+# Calcul du nombre d'adherents : -1 si on enlève Greg dans le nombre d'adherents
+# Jointure avec les cotisations id ? Pour le moment le compteur est effectue sur le libelle de la cotisation
+# Categorie tresorerie : manque les interets bancaires par rapport au budget, le site n'etait pas provisionne l'annee precedente
 # Attention le montant de la salle parait très très faible !!!
 
 
-# temps passé : 7h
+# temps passe : 7h
 ###############################################
-config <- config::get(file = "Bilan_KFR.yml")
+config <- config::get(file = "configuration.yml")
 ### Step 0 : initialisation
 kfr_token = config$kfr_token
 kfr_url = config$kfr_url
 
 ###############################################
 ### Step 1 : Chargement des librairies
-library(cronR)
+### needed ? library(cronR)
 library(httr) 
 library(jsonlite)
 library(sqldf)
@@ -28,6 +28,10 @@ library(config)
 
 
 generateReports <- function() {
+  
+  print('Collection data...')
+  
+  
   adherents <- jsonlite::fromJSON(content(httr::GET(url=sprintf("%s/association/adherents",kfr_url)
                                                       ,add_headers("X-Auth-Token"=kfr_token) )
                                                       ,type="text"), flatten = TRUE) 
@@ -52,12 +56,15 @@ generateReports <- function() {
                                                       ,add_headers("X-Auth-Token"=kfr_token))
                                                       ,type="text"), flatten = TRUE) 
   
-  
+  materiels <- jsonlite::fromJSON(content(httr::GET(url=sprintf("%s/materiels",kfr_url)
+                                                      ,add_headers("X-Auth-Token"=kfr_token))
+                                                      ,type="text"), flatten = TRUE) 
   ###############################################
   ### Step 3 : calcul des indicateurs
+  print('Creating dataSet...')
   #  3.1 Indicateurs globaux par saison 
-  # 3.1.1 Adhérents 
-  adherents <-unnest(adherents, saisons) # On multiplie les lignes par le nombre de saisons effectuées par adhérent (unlist saisons)
+  # 3.1.1 Adherents 
+  adherents <-unnest(adherents, saisons) # On multiplie les lignes par le nombre de saisons effectuees par adherent (unlist saisons)
   adherents$actif <- as.integer(adherents$actif)
   adherents$saison_courante <- as.integer(adherents$saison_courante)
   adherents$saison <- as.integer(adherents$saisons)
@@ -70,52 +77,64 @@ generateReports <- function() {
       from adherents
       group by saison")
   
-  # 3.1.2 Bénévolats
+  # 3.1.2 Benevolats
   kpi_saisons_ben <- sqldf("
-    select saison_id as saison,
-    ROUND(sum(heure)+ sum(minute)/60 ,0) as nb_heures
-    from benevolats
-    group by saison_id  
+  select 
+    saison_id as saison,
+    ROUND(sum(heure)+ sum(minute)/60.0 ,0) as nb_heures
+  from 
+    benevolats
+  group by 
+    saison_id  
   ")
   
   # 3.1.3 Evènements
   kpi_saisons_eve <- sqldf("
-          select saison_id as saison,
-          count(distinct id) as nb_evenement
-          from events
-          group by saison_id")
-  
-  # 3.1.4 Balance tresorerie
-  saisons <- saisons[,-which(names(saisons) == 'adherents')] # Exclusion de la colonne adherent, stockée sous type list
-  tresorerie$cheque <- as.integer(tresorerie$cheque)
-  
-  # CFL: 09/06/2019 : modification RG sur A_encaisser (= montant pointé dans le bilan) et A_regler ( = montant à pointer dans le bilan)
-  kpi_saisons_tres <- sqldf("
-  select
-  tre.saison_id,
-  sum(case when tre.etat_name in ('A encaisser','A regler') then tre.montant else null end) as retard,
-  sum(case when tre.etat_name in ('Encaisse','Regle') and tre.pointe =1 then tre.montant else null end) as a_encaisser,
-  sum(case when tre.etat_name in ('Regle') and tre.pointe =0 then tre.montant else null end) as a_regler,
-  sum(case when tre.etat_name in ('Encaisse','Regle') then tre.montant else null end) as tre_saison
-  from tresorerie tre
-  inner join saisons s on tre.saison_id = s.id
-  group by tre.saison_id
+  select 
+    saison_id as saison,
+    count(distinct id) as nb_evenement
+  from 
+    events
+  group by 
+    saison_id
   ")
   
-  #Nouvelle requête pour afficher le solde de la trésorerie en fin de saison (over (order by) non permis en SQLite)
+  # 3.1.4 Balance tresorerie
+  saisons <- saisons[,-which(names(saisons) == 'adherents')] # Exclusion de la colonne adherent, stockee sous type list
+  tresorerie$cheque <- as.integer(tresorerie$cheque)
+  
+  # CFL: 09/06/2019 : modification RG sur A_encaisser (= montant pointe dans le bilan) et A_regler ( = montant a pointer dans le bilan)
+  kpi_saisons_tres <- sqldf("
+  select
+    tre.saison_id,
+    sum(case when tre.etat_name in ('A reclamer') then tre.montant else null end) as retard,
+    sum(case when tre.etat_name in ('Encaisse','Regle') then tre.montant else null end) as tre_saison,
+    sum(case when tre.pointe == 1 then tre.montant else null end) as somme_pointe_saison,
+    sum(case when tre.pointe == 0 then tre.montant else null end) as somme_a_pointer_saison
+  from 
+    tresorerie tre
+  inner join saisons s on tre.saison_id = s.id
+  where
+    tre.etat_name not in ('Annulee')
+  group by 
+    tre.saison_id
+  ")
+  
+  #Nouvelle requête pour afficher le solde de la tresorerie en fin de saison (over (order by) non permis en SQLite)
   kpi_saisons_tres <- sqldf("Select c1.saison_id, 
     c1.retard,
-    c1.a_encaisser,
-    c1.a_regler,
+    c1.somme_pointe_saison,
+    c1.somme_a_pointer_saison,
     c1.tre_saison,
     sum(c2.tre_saison) as tre_fin_saison
   from kpi_saisons_tres c1, 
-      kpi_saisons_tres c2
-  where c1.saison_id >= c2.saison_id
+    kpi_saisons_tres c2
+  where 
+    c1.saison_id >= c2.saison_id
   group by c1.saison_id,
     c1.retard,
-    c1.a_encaisser,
-    c1.a_regler,
+    c1.somme_pointe_saison,
+    c1.somme_a_pointer_saison,
     c1.tre_saison
   order by c1.saison_id ")
   
@@ -123,26 +142,27 @@ generateReports <- function() {
   # 3.1.5 Nombre de cours sur Google Agenda ->to do en automatique, message d'erreur sur les credentials
   # https://www.googleapis.com/calendar/v3/calendars/kungfurennes@gmail.com/events
   
-  # 3.1.6 Merge des différents indicateurs
+  # 3.1.6 Merge des differents indicateurs
   kpi_saisons <- sqldf("
-    select s.nom,s.active,
-      adh.nb_adherents, adh.nb_adherents_actif, adh.nb_inscriptions_annuelles, adh.nb_inscriptions_trimestrielles,
-      ben.nb_heures,
-      eve.nb_evenement,
-      tres.retard, tres.a_encaisser, tres.a_regler, tres.tre_saison, tres.tre_fin_saison
-    from saisons s
-    left join kpi_saisons_adh adh on s.id = adh.saison
-    left join kpi_saisons_ben ben on s.id = ben.saison
-    left join kpi_saisons_eve eve on s.id = eve.saison
-    left join kpi_saisons_tres tres on s.id = tres.saison_id
-    order by s.nom desc
-    limit 5
-    "
-  )
+  select s.nom,s.active,
+    adh.nb_adherents, adh.nb_adherents_actif, adh.nb_inscriptions_annuelles, adh.nb_inscriptions_trimestrielles,
+    ben.nb_heures,
+    eve.nb_evenement,
+    tres.retard, tres.somme_pointe_saison, tres.somme_a_pointer_saison, tres.tre_saison, tres.tre_fin_saison
+  from 
+    saisons s
+  left join kpi_saisons_adh adh on s.id = adh.saison
+  left join kpi_saisons_ben ben on s.id = ben.saison
+  left join kpi_saisons_eve eve on s.id = eve.saison
+  left join kpi_saisons_tres tres on s.id = tres.saison_id
+  order by 
+    s.nom desc
+  limit 5
+  ")
   
-  # 3.2 Indicateurs de trésorerie détaillés
+  # 3.2 Indicateurs de tresorerie detailles
   
-  # 3.2.1 Trésorerie par catégorie
+  # 3.2.1 Tresorerie par categorie
   kpi_tresorerie_cat <- sqldf("
   select 
    rq.categorie_name,
@@ -157,12 +177,12 @@ generateReports <- function() {
       tre.categorie_name,
       tre.categorie_id,
       sum(tre.montant)as total_realise,
-      sum(case when tre.montant<0 then tre.montant else 0 end) as depense,
+      sum(case when tre.montant<=0 then tre.montant else 0 end) as depense,
       sum(case when tre.montant>=0 then tre.montant else 0 end) as recette
       from tresorerie tre
       inner join saisons s on tre.saison_id = s.id
       where s.active = 1
-        and tre.etat_name in ('Encaisse','Regle')
+        and tre.etat_name not in ('Annulee')
       group by tre.saison_id,tre.categorie_id,tre.categorie_name
   ) rq
   left join 
@@ -172,7 +192,7 @@ generateReports <- function() {
           tre.categorie_id,
           sum(tre.montant)as total_realise  
           from tresorerie tre
-          where tre.etat_name in ('Encaisse','Regle')
+          where tre.etat_name  not in ('Annulee')
           group by tre.saison_id,tre.categorie_id
       ) n1 on rq.saison_id = n1.saison_id  and rq.categorie_id = n1.categorie_id
   ")
@@ -189,21 +209,24 @@ generateReports <- function() {
     from kpi_tresorerie_cat
   ")
   
-  # 3.2.2 Evolution de la trésorerie mois par mois
+  # 3.2.2 Evolution de la tresorerie mois par mois
   kpi_tresorerie_evolution <- sqldf("
   select
-  date(date_creation,'start of month') as mois_date,
-  strftime('%d/%m/%Y', date(date_creation,'start of month')) as mois,
-   sum(case when tre.montant<0 then tre.montant else 0 end) as depense,
-  sum(case when tre.montant>=0 then tre.montant else 0 end) as recette,
-  sum(tre.montant) as variation_tresorerie,
-  min(kpi.tre_fin_saison - kpi.tre_saison) as solde_n1
-  from tresorerie tre
+    date(date_creation,'start of month') as mois_date,
+    strftime('%d/%m/%Y', date(date_creation,'start of month')) as mois,
+    sum(case when tre.montant<=0 then tre.montant else 0 end) as depense,
+    sum(case when tre.montant>=0 then tre.montant else 0 end) as recette,
+    sum(tre.montant) as variation_tresorerie,
+    min(kpi.tre_fin_saison - kpi.tre_saison) as solde_n1
+  from 
+    tresorerie tre
   inner join saisons s on tre.saison_id = s.id
   left join kpi_saisons_tres kpi on tre.saison_id = kpi.saison_id
-  where s.active = 1 
-      and tre.etat_name in ('Encaisse','Regle')
-  group by strftime('%d/%m/%Y', date(date_creation,'start of month'))
+  where 
+    s.active = 1 
+    and tre.etat_name not in ('Annulee')
+  group by 
+    strftime('%d/%m/%Y', date(date_creation,'start of month'))
   ")
   
   kpi_tresorerie_evolution <- sqldf("
@@ -212,25 +235,37 @@ generateReports <- function() {
     c1.recette,
     c1.variation_tresorerie,
     min(c1.solde_n1)+sum(c2.variation_tresorerie) as tresorerie_totale
-  from kpi_tresorerie_evolution c1, 
-      kpi_tresorerie_evolution c2
-  where c1.mois_date >= c2.mois_date
-  group by c1.mois, 
+  from 
+    kpi_tresorerie_evolution c1, 
+    kpi_tresorerie_evolution c2
+  where 
+    c1.mois_date >= c2.mois_date
+  group by 
+    c1.mois, 
     c1.depense,
     c1.recette,
     c1.variation_tresorerie
-  order by c1.mois_date ")
+  order by 
+    c1.mois_date
+  ")
   
   # 3.3 Liste exhaustive
   
-  # 3.3.1 Liste des événements de la saison en cours
+  # 3.3.1 Liste des evenements de la saison en cours
   events <- sqldf("
-   select description, commentaire, date_creation,
-      substr(date_creation, 9, 2) || '/' || substr(date_creation, 6,2) || '/' || substr(date_creation, 1,4) || ': ' || description as titre_event
-   from events eve
-   inner join saisons se on eve.saison_id= se.id
-   where se.active = 1
-    ")
+  select 
+    description, 
+    commentaire, 
+    date_creation,
+    substr(date_creation, 9, 2) || '/' || substr(date_creation, 6,2) || '/' || substr(date_creation, 1,4) || ': ' || description as titre_event
+  from 
+    events eve
+  inner join saisons se on eve.saison_id= se.id
+  where 
+    se.active = 1
+  order by 
+    date_creation
+  ")
   
   # 3.3.2 Liste des revenus de la saison
   liste_recette <- sqldf("
@@ -238,36 +273,83 @@ generateReports <- function() {
     tre.categorie_name,
     strftime('%d/%m/%Y', date_creation) as date,
     tre.description,
-    tre.montant
-  from tresorerie tre
+    tre.montant,
+    tre.pointe
+  from 
+    tresorerie tre
   inner join saisons s on tre.saison_id = s.id
-  where s.active = 1 
-      and tre.etat_name in ('Encaisse','Regle')
-      and tre.montant>=0
-  
+  where 
+    s.active = 1 
+    and tre.etat_name in ('A reclamer','A encaisser','Encaisse')
   ")
   
-  # 3.3.2 Liste des dépenses de la saison
+  # 3.3.2 Liste des depenses de la saison
   liste_depense <- sqldf("
   select
     tre.categorie_name,
     strftime('%d/%m/%Y', date_creation) as date,
     tre.description,
-    tre.montant
-  from tresorerie tre
+    tre.montant,
+    tre.pointe
+  from 
+    tresorerie tre
   inner join saisons s on tre.saison_id = s.id
-  where s.active = 1 
-      and tre.etat_name in ('Encaisse','Regle')
-      and tre.montant<0
+  where 
+    s.active = 1 
+    and tre.etat_name in ('A regler','Regle')
+  ")
   
+  # 3.4.0 Liste du materiels en vente
+  materiels_vente <- sqldf("
+   select 
+    nom as description, 
+    stock as unite, 
+    prix_vente as prix, 
+    stock * prix_vente as montant
+   from 
+    materiels
+   where 
+    association = 0 
+    AND stock <> 0
+  ")
+
+  # 3.4.1 Liste du materiels en association
+  materiels_association <- sqldf("
+   select 
+    nom as description, 
+    stock as unite, 
+    prix_achat as prix, 
+    stock * prix_achat as montant
+   from 
+    materiels
+   where 
+    association = 1 
+    AND stock <> 0
+  ")
+  # 3.5.1 Liste benevolats
+  benevoltas_heures <- sqldf("
+   select 
+    b.date_creation as Date, 
+    b.description as Description,  
+    (b.heure*60 + b.minute)/60.0 as _nb_heures, 
+    b.adherent_name as _nom,
+    (b.heure*60 + b.minute)/60.0 * 9.75 as _montant
+   from 
+    benevolats b
+   inner join saisons s on b.saison_id = s.id
+   where 
+    s.active = 1
+   order by 
+    b.date_creation
   ")
   
   # 4. Export des fichiers
+  print('Saving Data into Excels..')
   
-  # Tips : changer de répertoire si besoin
+  # Tips : changer de repertoire si besoin
   # setwd(dir = '/projet/home/cflouriot')
-  
-  wbbilan <- openxlsx::loadWorkbook(config$kfr_excel_template)
+  print(sprintf('  Updating %s ',config$kfr_excel_template_bilan))
+  wbbilan <- openxlsx::loadWorkbook(config$kfr_excel_template_bilan)
   openxlsx::removeWorksheet(wbbilan, sheet = "DataR")
   openxlsx::addWorksheet(wbbilan, "DataR")
   openxlsx::writeData(wbbilan, "DataR", kpi_saisons, startCol = 1, startRow = 1, rowNames = FALSE, colNames = TRUE)
@@ -275,14 +357,31 @@ generateReports <- function() {
   openxlsx::writeData(wbbilan, "DataR", events, startCol = 23, startRow = 1, rowNames = FALSE, colNames = TRUE)
   openxlsx::writeData(wbbilan, "DataR", kpi_tresorerie_evolution, startCol = 29, startRow = 1, rowNames = FALSE, colNames = TRUE)
   openxlsx::writeData(wbbilan, "DataR", liste_recette, startCol = 36, startRow = 1, rowNames = FALSE, colNames = TRUE)
-  openxlsx::writeData(wbbilan, "DataR", liste_depense, startCol = 41, startRow = 1, rowNames = FALSE, colNames = TRUE)
-  openxlsx::writeData(wbbilan, "DataR", kpi_tresorerie_dep, startCol = 46, startRow = 1, rowNames = FALSE, colNames = TRUE)
-  openxlsx::writeData(wbbilan, "DataR", kpi_tresorerie_rece, startCol = 49, startRow = 1, rowNames = FALSE, colNames = TRUE)
-  saveWorkbook(wbbilan, "Bilan_KFR.xlsx", overwrite = TRUE)
-  
-  # 5. Suppression des dataset temporaires
+  openxlsx::writeData(wbbilan, "DataR", liste_depense, startCol = 42, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  openxlsx::writeData(wbbilan, "DataR", kpi_tresorerie_dep, startCol = 48, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  openxlsx::writeData(wbbilan, "DataR", kpi_tresorerie_rece, startCol = 51, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  saveWorkbook(wbbilan, config$kfr_excel_template_bilan, overwrite = TRUE)
+  print(sprintf(' %s saved',config$kfr_excel_template_bilan))
+
+  # Suppression des dataset temporaires
   rm(kpi_saisons_adh,kpi_saisons_ben,kpi_saisons_eve,kpi_saisons_tres,kpi_tresorerie_evolution,liste_depense,liste_recette)
   rm(kpi_saisons,kpi_tresorerie_cat,events,adherents,saisons,cat_tresorerie,benevolats,tresorerie,kpi_tresorerie_dep, kpi_tresorerie_rece)
+  
+    
+  print(sprintf('  Updating %s ',config$kfr_excel_template_materiels))
+  wbmateriels <- openxlsx::loadWorkbook(config$kfr_excel_template_materiels)
+  openxlsx::removeWorksheet(wbmateriels, sheet = "DataR")
+  openxlsx::addWorksheet(wbmateriels, "DataR")
+  openxlsx::writeData(wbmateriels, "DataR", materiels_association, startCol = 1, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  openxlsx::writeData(wbmateriels, "DataR", materiels_vente, startCol = 6, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  openxlsx::writeData(wbmateriels, "DataR", benevoltas_heures, startCol = 12, startRow = 1, rowNames = FALSE, colNames = TRUE)
+  saveWorkbook(wbmateriels, config$kfr_excel_template_materiels, overwrite = TRUE)
+  print(sprintf('  %s saved',config$kfr_excel_template_materiels))
+  
+  # Suppression des dataset temporaires
+  rm(materiels_association, materiels_vente, materiels)
+  
+  
 }
 
 
@@ -299,7 +398,7 @@ if (!token_valide) {
   stop(sprintf('ERROR AUTHENTICATION (changer le token %s)',kfr_token))
 } else {
   generateReports()
-  sprintf('Generation %s DONE',config$kfr_excel_template)
+  print('Generation DONE.')
 }
 rm(token_valide)
 
